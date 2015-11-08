05e35beac0ff227a5b1b86d402c9145b9d012857
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-11-08 08:46:42 +0200

    release selenide 2.24

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "2.23";
+  private static final String LAST_SELENIDE_VERSION = "2.24";
 
   @Rule
   public ScreenShooter screenShooter = failedTests();
