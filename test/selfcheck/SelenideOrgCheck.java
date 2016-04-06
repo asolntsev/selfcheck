98bf9dab37a360783fc38733dac9e8ad205cc5e5
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-04-07 00:23:47 +0300

    released Selenide 3.5.1

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "3.5";
+  private static final String LAST_SELENIDE_VERSION = "3.5.1";
 
   @Rule
   public ScreenShooter screenShooter = failedTests();
