863f645f917114c02fa9d14dfa3f350fc0ddbe0d
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-06-25 00:02:26 +0300

    upgrade to Selenide 5.2.4

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -26,7 +26,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "5.2.3";
+  private static final String LAST_SELENIDE_VERSION = "5.2.4";
 
   @Rule
   public ScreenShooter screenShooter = failedTests();
