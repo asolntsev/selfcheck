1d1df2077b0b7379a808541037b0ba70cfdcf094
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-03-02 14:43:17 +0200

    upgrade to Selenide 5.8.0

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -26,7 +26,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "5.7.0";
+  private static final String LAST_SELENIDE_VERSION = "5.8.0";
 
   @Rule
   public ScreenShooter screenShooter = failedTests();
