80b81543e9a7bd5ed7e933438d37cad52de3af6c
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-22 10:05:33 +0300

    Selenide 5.24.4: upgrade to WDM 5.0.3

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -27,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith({LogTestNameExtension.class, ScreenShooterExtension.class})
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "5.24.3";
+  private static final String LAST_SELENIDE_VERSION = "5.24.4";
 
   @BeforeEach
   public void openPage() {
