3f501076a15780a9cbd1e8b8bab5bcb70e034097
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-29 23:55:53 +0300

    release Selenide 5.25.0

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -27,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith({LogTestNameExtension.class, ScreenShooterExtension.class})
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "5.24.4";
+  private static final String LAST_SELENIDE_VERSION = "5.25.0";
 
   @BeforeEach
   public void openPage() {
