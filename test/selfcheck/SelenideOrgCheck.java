8f5ba8a76788ea37e448ce9e204e97a1f6806843
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-05-19 19:50:08 +0300

    upgrade to Selenide 6.5.0

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -27,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith({LogTestNameExtension.class, ScreenShooterExtension.class})
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "6.4.0";
+  private static final String LAST_SELENIDE_VERSION = "6.5.0";
 
   @BeforeEach
   public void openPage() {
