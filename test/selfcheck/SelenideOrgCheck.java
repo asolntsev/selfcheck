fe798fe09580610b91419da780d651fa8febb8a1
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-09-06 09:39:01 +0300

    release Selenide 6.18.0

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -23,7 +23,7 @@ import static com.codeborne.selenide.files.FileFilters.withNameMatching;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "6.17.2";
+  private static final String LAST_SELENIDE_VERSION = "6.18.0";
 
   @Test
   public void selenideOrg() {
