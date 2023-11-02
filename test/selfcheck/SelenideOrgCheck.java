63df16f8ff5495f373fdb3c11b307fe68399b16d
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-11-02 09:34:41 +0200

    released Selenide 7.0.2

--- test/selfcheck/SelenideOrgCheck.java
+++ test/selfcheck/SelenideOrgCheck.java
@@ -23,7 +23,7 @@ import static com.codeborne.selenide.files.FileFilters.withNameMatching;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelenideOrgCheck {
-  private static final String LAST_SELENIDE_VERSION = "7.0.1";
+  private static final String LAST_SELENIDE_VERSION = "7.0.2";
   private static final WebElementCondition enabledAndVisible = new EnabledAndVisible();
 
   @Test
